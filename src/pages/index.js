8befb2b3fd4f4c31a9f95c40203b83becbf72383
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-12-08 23:39:06 +0600

    added new bannar

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
           alt=""
         /> */}
         <img
-          className="h-[600px] sm:h-[1050px]  mx-auto"
+          className="h-[600px] w-full sm:h-[1050px]  mx-auto"
           src="/images/foxhutHeroBg1.png"
           alt=""
         />
