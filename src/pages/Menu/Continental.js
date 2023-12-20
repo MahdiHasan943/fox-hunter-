2fa2ceb94af674d681b50389ef8b2ea0dd1e7de6
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-12-20 21:25:10 +0600

    fixed positon and also some issue about padding & margin

--- src/pages/Menu/Continental.js
+++ src/pages/Menu/Continental.js
@@ -18,7 +18,7 @@ const Continental = () => {
           African Section     </p>
 
           <div className="xl:w-[960px] mx-auto">
-            <div className="">
+            <div className=" relative">
               <div className="flex justify-between px-2">
                 <p className="MDPText dark:text-white text-black ">
                 Jollof Rice with Plantain & Salad 
@@ -26,31 +26,35 @@ const Continental = () => {
                 <p className="MDPText text-[#B5986D]"></p>
               </div>
               <img className="py-2 w-full" src="../../MDLine.png" alt="" />
-
+                    <img className="w-[200px] sm:absolute top-0 right-0 h-[150px]" src="../../images/cFirstImage.png" alt="" />
               <p className="MDSPText py-4 dark:text-[#ffffffb3] text-black">
               opt chicken, assorted meat, fish
                           </p>
             </div>
             {/* 2 */}
-            <div className="py-20">
+            <div className="py-[100px] relative">
               <div className="flex justify-between px-2">
                 <p className="MDPText dark:text-white text-black ">
                 Student Jollof Rice with Chicken
                 </p>
                 <p className="MDPText  py-4 text-[#B5986D]"></p>
               </div>
+              <img className="w-[200px]  sm:absolute top-[65px] right-0 h-[150px]" src="../../images/empty.png" alt="" />
+
               <img className="py-2 w-full" src="../../MDLine.png" alt="" />
               <p className="MDSPText dark:text-[#ffffffb3] text-black">
               </p>
             </div>
             {/* 3 */}
-            <div className="">
+            <div className="relative">
               <div className="flex justify-between px-2">
                 <p className="MDPText dark:text-white text-black ">
                 African Spiced Chicken Suya
                 </p>
                 <p className="MDPText text-[#B5986D]">  </p>
               </div>
+              <img className="w-[200px] sm:absolute top-0 right-0 h-[150px]" src="../../images/cThirdItem.png" alt="" />
+
               <img className="py-2 w-full" src="../../MDLine.png" alt="" />
 
               <p className="MDSPText py-4 dark:text-[#ffffffb3] text-black">Served with onions, tomato and spiced suya pepper
@@ -59,43 +63,49 @@ const Continental = () => {
             </div>
 
             {/* 4 */}
-            <div className="py-20">
+            <div className="my-[50px] relative">
               <div className="flex justify-between px-2">
                 <p className="MDPText dark:text-white text-black ">
                 African Spiced Beef Suya
                 </p>
                 <p className="MDPText text-[#B5986D]"></p>
               </div>
+              <img className="w-[200px] sm:absolute top-0 right-0 h-[150px]" src="../../images/cfourItem.png" alt="" />
+
               <img className="py-2 w-full" src="../../MDLine.png" alt="" />
 
               <p className="MDSPText py-4 dark:text-[#ffffffb3] text-black">Served with onions, tomato and spiced suya pepper
               </p>
             </div>
                       {/* 5 */}
-                      <div className="">
+                      <div className="relative">
               <div className="flex justify-between px-2">
                 <p className="MDPText dark:text-white text-black ">
                 African Spiced Beef Suya Kebab
                 </p>
                 <p className="MDPText text-[#B5986D]"></p>
               </div>
+              <img className="w-[200px] sm:absolute top-0 right-0 h-[150px]" src="../../images/cfiveItem.png" alt="" />
+
               <img className="py-2 w-full" src="../../MDLine.png" alt="" />
 
-              <p className="MDSPText py-4 dark:text-[#ffffffb3] text-black">served with salad, mayo, bbq, ketchup with home made hand cut chips
+              <p className="MDSPText md:w-[500px] py-4 dark:text-[#ffffffb3] text-black">served with salad, mayo, bbq, ketchup with home made hand cut chips
               </p>
                       </div>
                       
                       {/* 6 */}
-                      <div className="py-20">
+                      <div className="relative my-20">
               <div className="flex justify-between px-2">
                 <p className="MDPText dark:text-white text-black ">
                 On Bone Spice Grilled Tilapia Fish
                 </p>
                 <p className="MDPText text-[#B5986D]"></p>
               </div>
+              <img className="w-[200px] sm:absolute top-0 right-0 h-[150px]" src="../../images/csixItem.png" alt="" />
+
               <img className="py-2 w-full" src="../../MDLine.png" alt="" />
 
-              <p className="MDSPText py-4 dark:text-[#ffffffb3] text-black">Served with salad, African spiced fish sauce and options of plantain or potato wedges
+              <p className="MDSPText md:w-[500px] py-4 dark:text-[#ffffffb3] text-black">Served with salad, African spiced fish sauce and options of plantain or potato wedges
               </p>
             </div>
           </div>
